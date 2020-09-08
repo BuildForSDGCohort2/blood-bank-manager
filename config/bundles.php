913ab98ff30805dfe399e095f7c3f011d78a53ec
Joel Lusavuvu <lusavuvujoel39@gmail.com>
2020-09-08 19:20:05 +0100

    feat(app-auth): add and setup user-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Enigma972\UserBundle\Enigma972UserBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
