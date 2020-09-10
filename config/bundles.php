d95d260a35e94189e050e6fabf761224acf72484
Joel Lusavuvu <lusavuvujoel39@gmail.com>
2020-09-10 21:56:52 +0100

    build(deps): add flashy bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
